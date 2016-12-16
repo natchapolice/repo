a6a60f61389273af5423e0809a2614488d20d506
hall <lo.sinlapachai_st@tni.ac.th>
2016-12-16 03:34:17 +0000

    commit4

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
-class printLotto:
-    
-    def run_1st():
-        #code
-        
-        return lotto_1st
+import random
+
+number = random.randint(1, 999999)
+
+for i in range(1,9999999): 
+    if number > 99999:
+        print(number)
+        break
